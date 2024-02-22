706817bfbc24a0eed61c9b2fddf0943fedb1369b
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-22 11:15:29 +0100

    feat: env varible test

--- spec/match_keystore_android_action_spec.rb
+++ spec/match_keystore_android_action_spec.rb
@@ -1,5 +1,12 @@
 describe Fastlane::Actions::MatchKeystoreAction do
   describe '#run' do
+    before do
+      allow(ENV).to receive(:fetch).with('ANDROID_MATCH_DEBUG_KEYSTORE').and_return('path_to_debug_keystore')
+      allow(ENV).to receive(:fetch).with('ANDROID_MATCH_RELEASE_KEYSTORE').and_return('path_to_release_keystore')
+      allow(ENV).to receive(:fetch).with('ANDROID_MATCH_BRANCH').and_return('branch_name')
+      allow(ENV).to receive(:fetch).with('ANDROID_MATCH_URL').and_return('repository_url')
+    end
+
     it 'raises an error for invalid keystore type' do
       expect do
         Fastlane::Actions::MatchKeystoreAction.run(type: 'invalid', force: false)
@@ -7,7 +14,8 @@ describe Fastlane::Actions::MatchKeystoreAction do
     end
 
     it 'raises an error for missing keystore' do
-      allow(ENV).to receive(:[]).and_return(nil) # Stub environment variables
+      expect(File).to receive(:exist?).and_return(false)
+
       expect do
         Fastlane::Actions::MatchKeystoreAction.run(type: 'debug', force: false)
       end.to raise_error(RuntimeError, /Missing keystore/)
