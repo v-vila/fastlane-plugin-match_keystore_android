2a957acec1561ea50e8ab51941a4ca36ffb33588
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-22 10:26:34 +0100

    feat: test

--- spec/match_keystore_android_action_spec.rb
+++ spec/match_keystore_android_action_spec.rb
@@ -1,9 +1,27 @@
-describe Fastlane::Actions::MatchKeystoreAndroidAction do
+describe Fastlane::Actions::MatchKeystoreAction do
   describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The match_keystore_android plugin is working!")
+    it 'clones the repository and copies the keystore' do
+      allow(ENV).to receive(:[]).and_return(nil) # Stub environment variables
+      allow(FileUtils).to receive(:mkdir_p)
+      allow(FileUtils).to receive(:cp)
+      allow(FileUtils).to receive(:rm_rf)
+      allow(File).to receive(:exist?).and_return(false)
+      expect(Fastlane::Actions::MatchKeystoreAction).to receive(:sh).with(/git clone/)
 
-      Fastlane::Actions::MatchKeystoreAndroidAction.run(nil)
+      Fastlane::Actions::MatchKeystoreAction.run(type: 'debug', force: false)
+    end
+
+    it 'raises an error for invalid keystore type' do
+      expect do
+        Fastlane::Actions::MatchKeystoreAction.run(type: 'invalid', force: false)
+      end.to raise_error(RuntimeError, /Invalid type/)
+    end
+
+    it 'raises an error for missing keystore' do
+      allow(ENV).to receive(:[]).and_return(nil) # Stub environment variables
+      expect do
+        Fastlane::Actions::MatchKeystoreAction.run(type: 'debug', force: false)
+      end.to raise_error(RuntimeError, /Missing keystore/)
     end
   end
 end
