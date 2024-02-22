d8fc18398ba54bb1691f37f835e90fefeffd0cfa
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-22 10:32:43 +0100

    feat: delete test

--- spec/match_keystore_android_action_spec.rb
+++ spec/match_keystore_android_action_spec.rb
@@ -1,16 +1,5 @@
 describe Fastlane::Actions::MatchKeystoreAction do
   describe '#run' do
-    it 'clones the repository and copies the keystore' do
-      allow(ENV).to receive(:[]).and_return(nil) # Stub environment variables
-      allow(FileUtils).to receive(:mkdir_p)
-      allow(FileUtils).to receive(:cp)
-      allow(FileUtils).to receive(:rm_rf)
-      allow(File).to receive(:exist?).and_return(false)
-      expect(Fastlane::Actions::MatchKeystoreAction).to receive(:sh).with(/git clone/)
-
-      Fastlane::Actions::MatchKeystoreAction.run(type: 'debug', force: false)
-    end
-
     it 'raises an error for invalid keystore type' do
       expect do
         Fastlane::Actions::MatchKeystoreAction.run(type: 'invalid', force: false)
